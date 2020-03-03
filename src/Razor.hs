e2167772ef9c4dc1d5ce4c9b8867b89a2b851a7b
Jesse Williamson <Jesse@thunklife.local>
2020-03-02 18:57:20 -0800

    distributive property of multiplication

--- src/Razor.hs
+++ src/Razor.hs
@@ -4,15 +4,20 @@ data Exp = Lit Int | Add Exp Exp | Mul Exp Exp deriving (Show, Eq)
 
 eval :: Exp -> Int
 eval (Lit x) = x
-eval (Add exp1 exp2) = (eval exp1) + (eval exp2)
-eval (Mul exp1 exp2) = (eval exp1) * (eval exp2)
+eval (Add x y) = eval x + eval y
+eval (Mul x y) = eval x * eval y
 
 pretty :: Exp -> String
 pretty (Lit x) = show x
-pretty (Add exp1 exp2) = "( " ++ (pretty exp1) ++ " + " ++ (pretty exp2) ++ " )"
-pretty (Mul exp1 exp2) = "( " ++ (pretty exp1) ++ " * " ++ (pretty exp2) ++ " )"
+pretty (Add x y) = "( " ++ pretty x ++ " + " ++ pretty y ++ " )"
+pretty (Mul x y) = "( " ++ pretty x ++ " * " ++ pretty y ++ " )"
 
 distribute :: Exp -> Exp
-distribute _ = undefined
+distribute x@(Lit _) = x
+distribute (Add x y) = Add (distribute x) (distribute y)
+distribute (Mul (Add x y) z) = Add (Mul (distribute x) (distribute z)) (Mul (distribute y) (distribute z))
+distribute (Mul x (Add y z)) = Add (Mul (distribute y) (distribute x)) (Mul (distribute z) (distribute x))
+distribute (Mul x y) = Mul (distribute x) (distribute y)
+  
 --
 -- Mul (Add (Lit 1) (Lit 2)) Lit 5 == Add (Mul (Lit 5) (Lit 1)) (Mul (Lit 5) (Lit 2))
