3dcb4645d4abad706442ef8d08c2bc5aa1b1d119
Jesse Williamson <Jesse@thunklife.local>
2020-03-09 19:59:51 -0700

    basic parsing with chars & andThen

---
+++ src/Parser.hs
@@ -0,0 +1,25 @@
+module Parser where
+
+newtype Parser a = Parser (String -> Either String (a, String))
+
+pChar :: Char -> Parser Char
+pChar c = Parser pChar'
+  where 
+    pChar' [] = Left "Empty input"
+    pChar' s
+      | head s == c = Right (c, drop 1 s)
+      | otherwise = Left "Not Found"
+
+run :: Parser a -> String -> Either String (a, String)
+run (Parser fn) = fn
+
+(.>>.) :: Parser a -> Parser b -> Parser (a, b)
+(Parser pa) .>>. (Parser pb) = Parser andThen
+  where
+    andThen s =
+      case pa s of
+        (Left err) -> Left err
+        (Right (c1, rest1)) ->
+          case pb rest1 of 
+            (Left err) -> Left err
+            (Right (c2, rest2)) -> Right ((c1, c2), rest2)
